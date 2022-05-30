d7c1b38b70b54a0231b091fb16c62ae365d79323
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-05-30 10:39:12 +0200

    feat: enable service as a OAuth resource server

---
+++ src/main/kotlin/fr/polyflix/user/infrastructure/configuration/SecurityConfiguration.kt
@@ -0,0 +1,19 @@
+package fr.polyflix.user.infrastructure.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfiguration: WebSecurityConfigurerAdapter() {
+    override fun configure(web: HttpSecurity) {
+        http.authorizeRequests {
+            it
+                .antMatchers(HttpMethod.GET, "/users/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        }.oauth2ResourceServer { it.jwt() }
+    }
+}
