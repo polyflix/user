744061e096af9836769da28f5fdba10fdcd56413
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-05-30 23:00:40 +0200

    fix: update kafka user event schema

--- src/main/kotlin/fr/polyflix/user/domain/messaging/event/UserEvent.kt
+++ src/main/kotlin/fr/polyflix/user/domain/messaging/event/UserEvent.kt
@@ -2,20 +2,16 @@ package fr.polyflix.user.domain.messaging.event
 
 import fr.polyflix.user.domain.entity.User
 import fr.polyflix.user.domain.messaging.model.UserMessage
-import java.util.*
 
-data class UserEvent private constructor(val trigger: Trigger?, val id: UUID?, val fields: UserMessage?) {
+data class UserEvent private constructor(val trigger: Trigger?, val fields: UserMessage?) {
     data class Builder(
-        private var id: UUID? = null,
         private var trigger: Trigger? = null,
-        private var fields: UserMessage? = null
+        private var payload: UserMessage? = null
     ) {
         fun withTrigger(trigger: Trigger) = apply { this.trigger = trigger }
-        fun withId(id: UUID) = apply { this.id = id }
         fun withUser(user: User) = apply {
-            this.fields = UserMessage(user.id, user.email, user.username, user.firstName, user.lastName, user.avatar)
-            this.id = user.id
+            this.payload = UserMessage(user.id, user.email, user.username, user.firstName, user.lastName, user.avatar)
         }
-        fun build(): UserEvent = UserEvent(trigger, id, fields)
+        fun build(): UserEvent = UserEvent(trigger, payload)
     }
 }
