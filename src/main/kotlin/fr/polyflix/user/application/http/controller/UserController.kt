6cdfd93fb4e0e6fdba1162d0fbdd175ac58ada72
thomas.gouveia <thomas.gouveia@etu.umontpellier.fr>
2022-05-30 10:39:32 +0200

    feat: handle permissions on update route

--- src/main/kotlin/fr/polyflix/user/application/http/controller/UserController.kt
+++ src/main/kotlin/fr/polyflix/user/application/http/controller/UserController.kt
@@ -3,14 +3,19 @@ package fr.polyflix.user.application.http.controller
 import fr.polyflix.user.application.http.dto.request.user.UpdateUserRequest
 import fr.polyflix.user.application.http.dto.response.user.UserPaginatedResponse
 import fr.polyflix.user.application.http.dto.response.user.UserResponse
+import fr.polyflix.user.domain.enum.Roles
 import fr.polyflix.user.domain.service.UserService
+import org.apache.kafka.common.protocol.types.Field.Bool
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,6 +24,7 @@ import java.util.UUID
 @RestController
 @RequestMapping("/users")
 class UserController(private val userService: UserService) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping
     fun findAll(
@@ -40,16 +46,22 @@ class UserController(private val userService: UserService) {
     }
 
     @PutMapping("/{id}")
-    fun updateOn(@PathVariable id: String, @RequestBody body: UpdateUserRequest): ResponseEntity<UserResponse>? {
+    fun updateOn(@PathVariable id: String, @RequestBody body: UpdateUserRequest, @RequestHeader("X-User-Id") userId: String, @RequestHeader("X-User-Roles") userRoles: String): ResponseEntity<UserResponse>? {
+        val roles = userRoles.split(",")
+        if (!hasSufficientPermissions(id, userId, roles)) {
+            logger.warn("Forbidden update. $userId is not administrator or isn't the resource owner of the user : $id.")
+            return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .build()
+        }
+
         return userService
             .updateUser(UUID.fromString(id), body.username, body.firstName, body.lastName, body.avatar)
             .map {ResponseEntity.ok(UserResponse(it)) }
             .orElseGet { ResponseEntity.notFound().build() }
     }
 
-    @DeleteMapping("/{id}")
-    fun deleteOne(@PathVariable id: String): ResponseEntity<UserResponse>? {
-        userService.deleteUser(UUID.fromString(id))
-        return ResponseEntity.noContent().build()
+    private fun hasSufficientPermissions(subject: String, user: String, userRoles: List<String>): Boolean {
+        return subject == user || userRoles.contains(Roles.Administrator.name.uppercase())
     }
 }
